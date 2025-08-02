adb8a1a9ef5088e3a5f38c5455348244505945a8
Neo-nexVI <helixflow13@gmail.com>
2025-08-02 10:49:25 -0400

    hangman game alpha of hangman game complete next steps, read words from a while instead of entering words

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -1,12 +1,40 @@
-indivi_chars = []
+import getpass #moduel to hide user input
+
+word = []
+switch = []
+
+# word_choice = getpass.getpass(prompt = "Enter a Word: ").lower()
 word_choice = input("Enter a Word: ").lower()
 for x in range(len(word_choice) ):
-    init = word_choice[x]
-    indivi_chars.append(init)
-print(indivi_chars)
+    letter = word_choice[x]
+    word.append(letter); switch.append(letter)
 
-player_choice = input("chose a letter: ")
-if player_choice == indivi_chars[1]:
-    print("yes")
-else:
-    print("no")
+for y in range(len(switch)):
+    switch[y] = "_"
+    blanks = switch
+print(f"Enjoy The Game: {blanks}")
+
+count = 0
+tries = 6
+while True:
+    if "_" not in blanks:
+        print("You Won, the correctly guessed the word")
+        break
+    else:
+        choice = input("enter a letter: ").lower()
+
+        if choice in word:
+            for index in range(len(word)):
+                if word[index] == choice:
+                    print(f"{choice}: is found @ position:{index}")
+                    blanks[index] = choice
+                else:
+                    pass
+            print(blanks)       
+        else:
+            tries -=1
+            print(f"No letter found in word, try again. {tries} tries reamianing")
+            count += 1
+            if count == 6 or tries == 0:
+                print(f"Game Over. The correct word was: {word_choice}")
+                break
---
+++ hangman_game/main2.py
@@ -0,0 +1,4 @@
+word=[]
+blanks=[]
+player_word = input("enter a word: ").lower()
+print(player_word)
